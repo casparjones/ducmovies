46115d198d797743d9e1d7db2b5e1c91e2a99176
Frank Vlatten <frank@vlatten.biz>
2016-10-30 13:32:18 +0100

    adding buttons for film list

--- lib/DataHolder.py
+++ lib/DataHolder.py
@@ -18,6 +18,19 @@ class DataHolder:
 
             return result
 
+    def getFilmVerliehenInFiliale(self, film, filiale):
+        with self.connection.cursor() as cursor:
+            # Read a single record
+            sql = "SELECT * FROM `ausleih` AS a JOIN `mitarbeiter` AS m ON a.Mitarbeiter = m.Mitarbeiternummer " + \
+                " JOIN kunden AS k ON a.Kunde = k.Kundennummer" + \
+                " WHERE m.Filiale = " + str(filiale["Filialennummer"]) + " AND a.Film = " + str(film["Filmnummer"]) + \
+                " ORDER BY Datum DESC"
+            print(sql)
+            cursor.execute(sql)
+            result = cursor.fetchone()
+
+            return result
+
     def getKunden(self):
         with self.connection.cursor() as cursor:
             # Read a single record
@@ -26,6 +39,11 @@ class DataHolder:
             result = cursor.fetchall()
 
             return result
+
+    def delete_film(self, film):
+        with self.connection.cursor() as cursor:
+            sql = "DELETE FROM `filme` WHERE Filmnummer = " + str(film["Filmnummer"])
+            cursor.execute(sql)
 
     def getMitarbeiter(self):
         with self.connection.cursor() as cursor:
@@ -36,11 +54,14 @@ class DataHolder:
 
             return result
 
-    def getFilme(self, filiale):
+    def getFilme(self, filiale=None):
         with self.connection.cursor() as cursor:
             # Read a single record
-            sql = "SELECT * FROM `film_in_filiale` as fif join `filme` as f on fif.film_id = f.Filmnummer " + \
-                  "WHERE fif.filiale_id = " + str(filiale["Filialennummer"])
+            if filiale is None:
+                sql = "SELECT * FROM `filme`"
+            else:
+                sql = "SELECT * FROM `film_in_filiale` as fif join `filme` as f on fif.film_id = f.Filmnummer " + \
+                      "WHERE fif.filiale_id = " + str(filiale["Filialennummer"])
             print(sql)
             cursor.execute(sql)
             result = cursor.fetchall()
--- lib/FilmAppMenu.py
+++ lib/FilmAppMenu.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 from lib.DataHolder import DataHolder
+from tkinter.font import Font
 
 import tkinter.messagebox
 import tkinter
@@ -16,6 +17,8 @@ class FilmAppMenu(object):
         self.createmenu()
 
         self.mainframe = Frame(self.root, padding="6 6 12 12")
+
+        self.headerFont = Font(family="Helvetica", size=12)
 
         ## buttons
         ## self.close = Button(self.mainframe, text="Close",command=self.closeFrame).grid(column=1, row=9, sticky=SE)
@@ -57,6 +60,11 @@ class FilmAppMenu(object):
         self.mainframe.grid()
 
         row = 0
+        row += 1
+        Label(self.mainframe, text="Vorname", font=self.headerFont).grid(column=1, row=row, sticky=W)
+        Label(self.mainframe, text="Name", font=self.headerFont).grid(column=2, row=row, sticky=W)
+        Label(self.mainframe, text="Telefon", font=self.headerFont).grid(column=3, row=row, sticky=W)
+
         for kunde in kunden:
             row += 1
             Label(self.mainframe, text=kunde["Vorname"]).grid(column=1, row=row, sticky=W)
@@ -81,12 +89,57 @@ class FilmAppMenu(object):
         self.mainframe.grid()
 
         row = 0
+        row += 1
+        Label(self.mainframe, text="Vorname", font=self.headerFont).grid(column=1, row=row, sticky=W)
+        Label(self.mainframe, text="Name", font=self.headerFont).grid(column=2, row=row, sticky=W)
+        Label(self.mainframe, text="Gehalt", font=self.headerFont).grid(column=3, row=row, sticky=W)
+        Label(self.mainframe, text="Filiale", font=self.headerFont).grid(column=4, row=row, sticky=W)
+
         for eachMitarbeiter in mitarbeiter:
             row += 1
             Label(self.mainframe, text=eachMitarbeiter["Vorname"]).grid(column=1, row=row, sticky=W)
             Label(self.mainframe, text=eachMitarbeiter["Name"]).grid(column=2, row=row, sticky=W)
             Label(self.mainframe, text=eachMitarbeiter["Gehalt"]).grid(column=3, row=row, sticky=W)
             Label(self.mainframe, text=eachMitarbeiter["Filiale"]).grid(column=4, row=row, sticky=W)
+
+    def delete_film(self, film):
+        self.dh.delete_film(film)
+        self.show_filme()
+
+    def show_filme(self):
+        filme = self.dh.getFilme()
+
+        ## build frame
+        for widget in self.mainframe.winfo_children():
+            widget.destroy()
+
+        self.mainframe.grid_remove()
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
+        self.mainframe.pack()
+        ## text labels
+        self.mainframe.pack(side="bottom", fill=BOTH, expand=True)
+        self.mainframe.grid()
+
+        row = 0
+        row += 1
+        Label(self.mainframe, text="Titel", font=self.headerFont).grid(column=1, row=row, sticky=W)
+        Label(self.mainframe, text="Genre", font=self.headerFont).grid(column=2, row=row, sticky=W)
+        Label(self.mainframe, text="Erscheinungsjahr", font=self.headerFont).grid(column=3, row=row, sticky=W)
+        Label(self.mainframe, text="Löschen", font=self.headerFont).grid(column=4, row=row, sticky=W)
+        Label(self.mainframe, text="Editieren", font=self.headerFont).grid(column=5, row=row, sticky=W)
+
+        for film in filme:
+            row += 1
+            Label(self.mainframe, text=film["Titel"]).grid(column=1, row=row, sticky=W)
+            Label(self.mainframe, text=film["Genre"]).grid(column=2, row=row, sticky=W)
+            Label(self.mainframe, text=film["Erscheinungsjahr"]).grid(column=3, row=row, sticky=W)
+            Button(self.mainframe, text="Löschen", command=lambda film=film: self.delete_film(film)).grid(column=4, row=row, sticky=W)
+            Button(self.mainframe, text="Editieren").grid(column=5, row=row, sticky=W)
+
+        row += 2
+        Button(self.mainframe, text="Neuer Film anlegen").grid(column=1, row=row, columnspan=5, sticky=E)
 
     def show_movies(self):
         print(self.filale)
@@ -105,17 +158,26 @@ class FilmAppMenu(object):
         self.mainframe.rowconfigure(0, weight=1)
         self.mainframe.pack()
         ## text labels
-        # Label(self.mainframe, text=self.filiale_name(self.filiale), font=("Helvetica", 32)).grid(column=1, row=1, sticky=E)
         self.mainframe.pack(side="bottom", fill=BOTH, expand=True)
         self.mainframe.grid()
 
         row = 0
+        row += 1
+        Label(self.mainframe, text="Titel", font=self.headerFont).grid(column=1, row=row, sticky=W)
+        Label(self.mainframe, text="Genre", font=self.headerFont).grid(column=2, row=row, sticky=W)
+        Label(self.mainframe, text="Erscheinungsjahr", font=self.headerFont).grid(column=3, row=row, sticky=W)
+        Label(self.mainframe, text="Verliehen?", font=self.headerFont).grid(column=4, row=row, sticky=W)
 
         for film in filme:
             row += 1
             Label(self.mainframe, text=film["Titel"]).grid(column=1, row=row, sticky=W)
             Label(self.mainframe, text=film["Genre"]).grid(column=2, row=row, sticky=W)
             Label(self.mainframe, text=film["Erscheinungsjahr"]).grid(column=3, row=row, sticky=W)
+            verliehen = self.dh.getFilmVerliehenInFiliale(film, self.filale)
+            if verliehen is None:
+                Label(self.mainframe, text=" ./. ").grid(column=4, row=row, sticky=W)
+            else:
+                Label(self.mainframe, text=verliehen["Datum"]).grid(column=4, row=row, sticky=W)
 
     def filiale_name(self, filiale):
         return "Nummer " + str(filiale['Filialennummer']) + " " + filiale['Strasse']
@@ -131,7 +193,8 @@ class FilmAppMenu(object):
                                  command=lambda filiale=filiale: self.open_filiale(filiale))
 
         helpmenu = Menu(menu)
-        menu.add_cascade(label="Weiter", menu=helpmenu)
+        menu.add_cascade(label="Grunddaten", menu=helpmenu)
         helpmenu.add_command(label="Kunden", command=self.show_kunden)
         helpmenu.add_command(label="Mitarbeiter", command=self.show_mitarbeiter)
+        helpmenu.add_command(label="Filme", command=self.show_filme)
         helpmenu.add_command(label="About...", command=self.about)
